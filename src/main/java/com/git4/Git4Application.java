eb6ae5341af030871ef2b275d9aa2a1aa30c9cd9
nilu <ndebankura01@gmail.com>
2025-06-05 10:08:31 +0530

    first commit

---
+++ src/main/java/com/git4/Git4Application.java
@@ -0,0 +1,14 @@
+package com.git4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git4Application.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/git4/Git4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
